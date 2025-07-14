3fdda11757aa4323d5734d7f423bf1b6a1664715
jkim68888 <jkim68888@gmail.com>
2025-07-14 21:29:07 +0900

    feat: 미디어 & 위치 권한 설정

--- utils/locationUtil.ts
+++ utils/locationUtil.ts
@@ -1,9 +1,18 @@
 import * as Location from 'expo-location';
+import * as MediaLibrary from 'expo-media-library';
 
 // 위치 정보를 텍스트로 변환
 export const getLocationText = async (latitude: number, longitude: number) => {
   try {
+    const { status } = await MediaLibrary.requestPermissionsAsync();
+    
+    if (status !== 'granted') {
+      console.log('미디어 권한이 거부되었습니다.');
+      return undefined;
+    }
+
     const result = await Location.reverseGeocodeAsync({ latitude, longitude });
+
     if (result.length > 0) {
       const address = result[0];
       return `${address.city || ''} ${address.district || ''} ${address.street || ''}`.trim();
